9b3fd2bd462cacacb67480e074144661f0f656f8
aka <aka1w4@proton.me>
2025-09-01 07:57:02 +0000

    add player focus, animation, func move

--- player.cpp
+++ player.cpp
@@ -3,34 +3,62 @@
 #include <raylib.h>
 
 float width = 16;
-float height = 16;
+float height = 32;
+int frameCount = 4;
 
 Player::Player(float x, float y) : pos(Vector2{x, y}) {
-   img = LoadTexture("assets/16x16idle.png");
+   img[0] = LoadTexture("assets/16x32idle.png");
+   img[1] = LoadTexture("assets/16x32walk.png");
 }
 
 Player::~Player() {
-  UnloadTexture(img);
+  for (auto &i : img) {
+    UnloadTexture(i);
+  }
+}
+
+void Player::Move(float dx, float dy, Focus df, bool right) {
+  if (dx != 0 || dy != 0) {
+    if (ms == isIdle) {
+      ms = isWalk;
+    }
+
+    pos.x += dx;
+    pos.y += dy;
+    f = df;
+    facingright = right;
+  } else {
+    ms = isIdle;
+  }
 }
 
 void Player::Update() {
+  count++;
   if (IsKeyDown(KEY_W)) {
-    pos.y -= 5;
+    Move(0, -5, Up, false);
   } else if (IsKeyDown(KEY_S)) {
-    pos.y += 5;
+    Move(0, 5, Down, false);
   } else if (IsKeyDown(KEY_D)) {
-    pos.x += 5;
+    Move(5, 0, Left, true);
   } else if (IsKeyDown(KEY_A)) {
-    pos.x -= 5;
+    Move(-5, 0, Left, false);
+  } else {
+    ms = isIdle;
   }
 }
 
 void Player::Drawing() {
-   Rectangle src = Rectangle{0, 0, width, height};
+  int x0 = (count / 8) % frameCount * width;
+  if (facingright) {
+    width = -16;
+  } else {
+    width = 16;
+  }
+   Rectangle src = Rectangle{(float)x0, height*f, width, height};
    Rectangle dst = Rectangle{pos.x, pos.y, width*3, height*3};
    Vector2 origin = Vector2{0,0};
 
-   DrawTexturePro(img, src, dst, origin, 0.0f, WHITE);
+   DrawTexturePro(img[ms], src, dst, origin, 0.0f, WHITE);
 
    std::cout << "game sedang berjalan: " << std::endl;
 }
--- player.hpp
+++ player.hpp
@@ -1,17 +1,35 @@
 #ifndef PLAYER
 #define PLAYER
 
+#include <array>
 #include <raylib.h>
+
+enum Focus {
+  Down,
+  Left = 2, 
+  Up = 4,
+};
+
+enum MoveState {
+  isIdle,
+  isWalk,
+};
+
 class Player {
   private:
     Vector2 pos;
-    Texture2D img;
+    std::array<Texture2D, 2> img;
+    unsigned int count;
+    Focus f = Down;
+    MoveState ms;
+    bool facingright; 
 
   public:
     Player(float x, float y);
     ~Player();
     void Update();
     void Drawing();
+    void Move(float dx, float dy, Focus df, bool right);
 };
 
 #endif // !PLAYER
