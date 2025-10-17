37a9652bb63ebfa2f52097a6f8309c9b266efa94
aka <aka1w4@proton.me>
2025-10-17 07:47:57 +0000

    update camera

--- src/camera/camera.cpp
+++ src/camera/camera.cpp
@@ -3,22 +3,22 @@
 #include <algorithm>
 
 void CameraGame::Update(Vector2 playerpos) {
-  float ViewW = (GetScreenWidth() * 0.5f)  / cam.zoom;
-  float ViewH = (GetScreenHeight() * 0.5f) / cam.zoom;
+  float halfViewW = cam.offset.x / cam.zoom;
+  float halfViewH = cam.offset.y / cam.zoom;
 
-  float minCamX = ViewW;
-  float maxCamX = mapMax - ViewW;
-  float minCamY = ViewH;
-  float maxCamY = mapMax - ViewH;
+  float minCamX = halfViewW;
+  float maxCamX = mapMax - halfViewW;
+  float minCamY = halfViewH;
+  float maxCamY = mapMax - halfViewH;
 
   if (mapMax <= GetScreenWidth() / cam.zoom) {
-    cam.target.x = mapMax * 0.5f;
+    cam.target.x = mapMax / 2.0f;
   } else {
     cam.target.x = std::clamp(playerpos.x, minCamX, maxCamX);
   }
 
   if (mapMax <= GetScreenHeight() / cam.zoom) {
-    cam.target.y = mapMax * 0.5f;
+    cam.target.y = mapMax / 2.0f;
   } else {
     cam.target.y = std::clamp(playerpos.y, minCamY, maxCamY);
   }
